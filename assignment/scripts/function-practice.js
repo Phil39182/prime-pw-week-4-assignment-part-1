4672b9a821f83faf31eb9015a8b9c4617ca50480
Josh Snyder <joshrsnyder@gmail.com>
2022-04-23 13:16:43 -0500

    up to Q10

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -105,10 +105,10 @@ function multiplyThree(num0, num1, num2 ){
 
 
 // 5. Function that will return true if a number is positive, 
-//    or greater than zero, and false otherwise
+//    or greater than zero, and false otherwise 
 function isPositive( number ) {
-  if ( number >= 0 ){
-    //if number is zero or higer will return true
+  if ( number > 0 ){
+    //if number is greater than zero will return true
     return true;
   }
   else{
@@ -118,22 +118,39 @@ function isPositive( number ) {
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive run with - should say true', isPositive(3) );
+console.log( 'isPositive - should say true', isPositive(3) );
 console.log( 'isPositive - should say false', isPositive(0) );
 console.log( 'isPositive - should say false', isPositive(-3) );
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
 
+let array = ['phil','monica','matt'];
+
+function getLast( array ) {
+  if ( array.length > 0 ) {
+    //returns last element in array
+    return array.slice(-1);
+  }
+  else {
+    //if array is empty, returns undefined
+    return 'undefined';
+  }
 }
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
-  
+  for ( let i = 0; i < array.length; i++ ) {
+    if ( value === array[i] ) {
+      return true;
+    }
+    else {
+      return false;
+    } 
+  }
 }
 
 // ----------------------
@@ -142,21 +159,42 @@ function find( value, array ){
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  //checks character at beginning of string using charAt()
+  if ( string.charAt([0]) === letter ) {
+    return true;
+  }
+  else { 
+    return false;
+  }
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+
+let numbers = [ 2, 4, 6, 8, 10, -3, -2 ];
+
+function sumAll( array ) {
   let sum = 0
+  
   // TODO: loop to add items
+
+  for ( let number of numbers ) {
+    sum += number;
+  }
+
   return sum;
 }
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+
+function positiveNumbers( numbers ){
+  for ( i = 0; numbers[i] > 0; i++ ) {
+      console.log(numbers[i])
+    }
+  }
 
 
 
